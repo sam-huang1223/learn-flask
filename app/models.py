5d27afcdfb0e07d8de41e61a48c9e15f14d9a56b
Sam Huang <samuel.huang1223@gmail.com>
2017-12-03 04:28:28 -0500

    initial commit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):  # tells python how to print objects of this class
+        return '<User %r>' % (self.nickname)
---
+++ app/views.py
@@ -0,0 +1,34 @@
+'''
+The views are the handlers that respond to requests from web browsers or other clients
+    Handlers are python functions
+    Each view function is mapped to one or more request URLs
+'''
+from flask import render_template, flash, redirect
+# The flash function is a quick way to show a message on the next page presented to the user
+from app import app  # importing object from current module
+from .forms import LoginForm  # importing class from another file in current module
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', posts=posts, user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
